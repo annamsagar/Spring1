00ef60e81d93ab6b857a0861613a1ee8d852875b
annamsagar <annamsagar05@gmail.com>
2026-05-17 13:55:14 +0530

    obj created

--- src/main/java/com/livein/Aliean.java
+++ src/main/java/com/livein/Aliean.java
@@ -1,6 +1,9 @@
 package com.livein;
 
 public class Aliean {
+    Aliean(){
+        System.out.println("Aliean object created");
+    }
     public void code(){
         System.out.println("hey i am code");
     }
---
+++ src/main/java/com/livein/Laptop.java
@@ -0,0 +1,10 @@
+package com.livein;
+
+public class Laptop {
+    Laptop(){
+        System.out.println("Laptop object created");
+    }
+    public void lap(){
+        System.out.println("Laptop");
+    }
+}
